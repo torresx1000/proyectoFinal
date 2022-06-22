2fb7caa1ca3e23e7a3dc90095c9f0ec6c6b22472
Osni Addin Torres Flores <N00215210@upn.pe>
2022-06-22 03:21:40 -0500

    ya se añadio referencias

--- principal.Designer.cs
+++ principal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace proyectoFinal
 {
-    partial class Form1
+    partial class principal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(815, 481);
+            this.Name = "principal";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- principal.cs
+++ principal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace proyectoFinal
 {
-    public partial class Form1 : Form
+    public partial class principal : Form
     {
-        public Form1()
+        public principal()
         {
             InitializeComponent();
         }
